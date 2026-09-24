c58e817b469add9c5975462334cfa52737975d50
SB90-CH <68231803+SB90-CH@users.noreply.github.com>
2026-09-24 18:51:01 +0200

    Refactor map.js to add multiple markers for posts

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -8,14 +8,11 @@
 // Karte erstellen
 // ---------------------------------------------------------
 
-// Mittelpunkt der Karte
-// Diese Koordinaten sind vorerst ein Beispiel.
-// Später setzen wir hier die genaue Position des Sternenwegs.
-const map = L.map("map").setView([47.2, 8.3], 13);
+const map = L.map("map");
 
 
 // ---------------------------------------------------------
-// OpenStreetMap als Kartenhintergrund laden
+// OpenStreetMap als Kartenhintergrund
 // ---------------------------------------------------------
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -29,12 +26,119 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 
 // ---------------------------------------------------------
-// Test-Marker
+// Posten des Sternenwegs
 // ---------------------------------------------------------
 
-const testMarker = L.marker([47.2, 8.3]).addTo(map);
+const posten = [
 
-testMarker.bindPopup(`
-    <strong>Sternenweg Rotzenwil</strong><br>
-    Hier kommt später ein Posten hin.
-`);
+    {
+        nummer: 1,
+        name: "Posten 1",
+        lat: 47.5139963,
+        lng: 9.2891543
+    },
+
+    {
+        nummer: 2,
+        name: "Posten 2",
+        lat: 47.5149390,
+        lng: 9.2937150
+    },
+
+    {
+        nummer: 3,
+        name: "Posten 3",
+        lat: 47.5159114,
+        lng: 9.2864308
+    },
+
+    {
+        nummer: 4,
+        name: "Posten 4",
+        lat: 47.5186137,
+        lng: 9.2847952
+    },
+
+    {
+        nummer: 5,
+        name: "Posten 5",
+        lat: 47.5190610,
+        lng: 9.2870864
+    },
+
+    {
+        nummer: 6,
+        name: "Posten 6",
+        lat: 47.5192892,
+        lng: 9.2902833
+    },
+
+    {
+        nummer: 7,
+        name: "Posten 7",
+        lat: 47.5180887,
+        lng: 9.2917702
+    },
+
+    {
+        nummer: 8,
+        name: "Posten 8",
+        lat: 47.5162994,
+        lng: 9.2934937
+    },
+
+    {
+        nummer: 9,
+        name: "Posten 9",
+        lat: 47.5145557,
+        lng: 9.2914390
+    },
+
+    {
+        nummer: 10,
+        name: "Posten 10",
+        lat: 47.5149345,
+        lng: 9.2962445
+    }
+
+];
+
+
+// ---------------------------------------------------------
+// Marker erstellen
+// ---------------------------------------------------------
+
+const markerGroup = L.featureGroup();
+
+posten.forEach(posten => {
+
+    const marker = L.marker([
+        posten.lat,
+        posten.lng
+    ]);
+
+    marker.bindPopup(`
+        <strong>${posten.name}</strong>
+        <br>
+        Sternenweg Rotzenwil
+    `);
+
+    marker.addTo(markerGroup);
+
+});
+
+
+// Marker-Gruppe zur Karte hinzufügen
+markerGroup.addTo(map);
+
+
+// ---------------------------------------------------------
+// Karte automatisch auf alle Posten ausrichten
+// ---------------------------------------------------------
+
+map.fitBounds(
+    markerGroup.getBounds(),
+    {
+        padding: [40, 40]
+    }
+);
